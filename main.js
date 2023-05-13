a643e1f1a90ac9a114b7dc96e69c20ae185aca79
KevinMartinez45 <martinezperezkevin@cticoban.edu.gt>
2023-05-12 20:32:06 -0600

    Commit inicial

---
+++ main.js
@@ -0,0 +1,21 @@
+
+function verificar(){
+    
+    var user = document.getElementById('user').value;
+    var pass = document.getElementById('pass').value;
+
+    if(user == "admin" && pass == "admin"){
+        
+        alert("Bienvenido");
+        document.getElementById('pass').value = "";
+        document.getElementById('user').value = "";
+        location.href = ('inicio.html');
+
+    }else{
+        
+        document.querySelector('.Wrong').classList.toggle('show');
+        document.getElementById('pass').value = "";
+        
+    }
+}
+
